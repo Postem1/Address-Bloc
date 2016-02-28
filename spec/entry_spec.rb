93524c812433a3e85103514bf629c1cb1314a929
Steeve Postemus <spostemus@gmail.com>
2016-02-27 17:33:09 -0800

    added controllers

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../models/entry'
 
 RSpec.describe Entry do
+
   describe "attributes" do
     let(:entry) { Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
 
@@ -29,7 +30,9 @@ RSpec.describe Entry do
     end
 
       describe "#to_s" do
+
            it "prints an entry as a string" do
+             
              entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
              expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
 
